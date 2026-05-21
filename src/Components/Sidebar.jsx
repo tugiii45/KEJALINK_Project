39ca0f7e3075926e884efade35766002f01924c2
Conrad <conradkaranja5@gmail.com>
2026-05-21 15:04:08 +0300

    Add useDispatch hook in Sisebar.jsx

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -24,17 +24,24 @@
 
 import React from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../Features/AuthSlice'
+
 
 function Sidebar() {
   const navigate = useNavigate()
 
+  const dispatch = useDispatch()
   const { user } = useSelector((state) => state.auth)
   const role = user?.role || 'tenant'
 
   const handleLogout = () => {
-    navigate('/')
+    dispatch(logout())
+    navigate('/', { replace: true })
   }
+
+
+
 
 
   return (
